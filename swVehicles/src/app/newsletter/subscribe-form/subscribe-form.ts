bc6ccb70a1eb5e02751fb03693848b9f1d514850
loran <laurent.muller@wizbii.com>
2025-12-18 13:05:00 +0100

    Feat - Enhance subscribe form with validation and reactive features

--- swVehicles/src/app/newsletter/subscribe-form/subscribe-form.ts
+++ swVehicles/src/app/newsletter/subscribe-form/subscribe-form.ts
@@ -1,5 +1,16 @@
-import { Component, effect, signal } from '@angular/core';
-import { email, Field, form, max, min, minLength, required, schema } from '@angular/forms/signals';
+import { Component, computed, effect, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  email,
+  Field,
+  FieldState,
+  form,
+  max,
+  min,
+  minLength,
+  required,
+  schema,
+} from '@angular/forms/signals';
 
 type SubscribeFormModel = {
   firstName: string;
@@ -15,9 +26,10 @@ const initialData: SubscribeFormModel = {
   yearsAsFan: NaN,
 };
 
+// Declaration des règles de validation.
 const subscriptionSchema = schema<SubscribeFormModel>((rootPath) => {
   required(rootPath.email, {
-    message: 'your email address is required to receive our newsletter.',
+    message: 'your email address is required to receive our newsletter.', // ici on déclare le message d'erreur personnalisé pour le champ email et l'erreur required
   });
   email(rootPath.email, {
     message: 'please enter a valid email address.',
@@ -34,17 +46,19 @@ const subscriptionSchema = schema<SubscribeFormModel>((rootPath) => {
 });
 
 @Component({
-  imports: [Field],
+  imports: [Field, ReactiveFormsModule],
   template: `
     <div class="card">
-      <!-- <div class="card-header">
-        {{ pageHeader() }}
-      </div> -->
+      <div class="card-header">
+        @let headerText = "Subscribe to our Newsletter " + fullName() ;
+        <h2>{{ headerText }}</h2>
+      </div>
       <div class="card-body">
         <form>
           <div class="row">
             <label class="row-label" for="firstNameId"> First Name </label>
             <div class="row-value">
+              <!-- [field] => Liaison du champ de formulaire au modèle. -->
               <input
                 type="text"
                 class="form-control"
@@ -81,6 +95,7 @@ const subscriptionSchema = schema<SubscribeFormModel>((rootPath) => {
                 placeholder="Email (required)"
               />
             </div>
+            <!-- Affichage des messages d'erreur de validation pour le champ email. -->
             @if (subscribeForm.email().invalid() && subscribeForm.email().touched()) {
             <div class="alert alert-danger">
               @for (error of subscribeForm.email().errors(); track error.kind) {
@@ -110,36 +125,54 @@ const subscriptionSchema = schema<SubscribeFormModel>((rootPath) => {
             }
           </div>
 
-          <!-- <div class="row">
+          <div class="row">
             <div class="row-value">
               <button
                 type="button"
                 title="Ensure the form is valid and required fields are entered before subscribing"
                 class="btn btn-primary"
-                (click)="subscribe()"
+                (click)="submit(subscribeForm())"
               >
                 Subscribe
               </button>
               <button type="button" class="btn btn-secondary" (click)="cancel()">Cancel</button>
             </div>
-          </div> -->
+          </div>
         </form>
 
-        <!-- @if (subscribeMessage()) {
+        @if (subscribeMessage()) {
         <div class="alert alert-success">
           {{ subscribeMessage() }}
         </div>
-        } -->
+        }
       </div>
     </div>
   `,
   styleUrls: ['./subscribe-form.css'],
 })
 export class SubscribeForm {
-  readonly #subscribeModel = signal<SubscribeFormModel>(initialData);
-  protected readonly subscribeForm = form(this.#subscribeModel, subscriptionSchema);
+  readonly #subscribeModel = signal<SubscribeFormModel>(initialData); // Le modèle de données du formulaire en tant que signal.
+  protected readonly subscribeForm = form(this.#subscribeModel, subscriptionSchema); // Le formulaire avec le modèle et le schéma de validation.
+
+  protected fullName = computed(() => {
+    const { firstName, lastName } = this.#subscribeModel();
+    return `${firstName} ${lastName}`.trim();
+  });
+
+  protected subscribeMessage = signal<string | null>(null);
 
   eff = effect(() => {
-    console.log('Subscribe Form Model Changed:', this.#subscribeModel());
+    console.log('Subscribe Form Model Changed:');
   });
+
+  protected cancel = (): void => this.subscribeForm().reset(initialData);
+
+  protected submit(form: FieldState<SubscribeFormModel>): void {
+    if (form.invalid()) {
+      form.markAsTouched();
+    }
+    this.subscribeMessage.set(`Thank you for subscribing, ${this.fullName() || 'Valued Fan'}!`);
+
+    this.subscribeForm().reset(initialData);
+  }
 }
